3e4222cd9a3c261eabfe3694e69d7eddaef305f3
Iury Magalhaes <iurylei@hotmail.com>
2018-05-31 19:49:17 -0300

    passando valores para o componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import Component from './component'
 
 //Precisa iniciar novamente o servidor - npm run dev
-ReactDOM.render(<Component />, document.getElementById('app')) //Gera o conteúdo dinamicamente
+ReactDOM.render(<Component value="Show"/>, document.getElementById('app')) //Gera o conteúdo dinamicamente
