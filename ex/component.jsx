0be795c503c7fe9350f2dc4f341ca71f9a838af9
Iury Magalhaes <iurylei@hotmail.com>
2018-05-31 19:51:37 -0300

    Criando e exportando dois componentes

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default props => (
-    <h1>{props.value}</h1> 
+const Primeiro = props => (
+    <h1> Primeiro Componente </h1>
 )
 
+const Segundo = props => (
+    <h2> Segundo Componente </h2>
+)
+
+export { Primeiro, Segundo }
