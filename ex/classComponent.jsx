9acd5daf3503da71491fa182af08956ef42eda2f
Iury Magalhaes <iurylei@hotmail.com>
2018-06-02 00:52:59 -0300

    Ajustes

--- ex/classComponent.jsx
+++ ex/classComponent.jsx
@@ -11,16 +11,28 @@ export default class ClassComponent extends Component{
         this.setState({ value: this.state.value + delta }) //Evolui o estado do value
     }
 
+    multiplicar(valor){
+        this.setState({ value: this.state.value * valor})
+    }
+
+    dividir(valor){
+        this.setState({ value: this.state.value / valor})
+    }
+
     render() {
         return (
             <div>
                 <h1>{ this.props.label }</h1>
                 <h2>{ this.state.value }</h2> 
 
-                <input type='text'/><br/><br />
-
                 <button onClick={ () => this.sum(-1) }> Desc </button>
                 <button onClick={ () => this.sum(1) }> Acr </button>
+                <br /><br />
+                <button onClick={ () => this.multiplicar(2) }> Multiplicar </button>
+                <button onClick={ () => this.dividir(2) }> Dividir </button>
+
+                <br /><br />
+                <input type='text'/><br/><br />
             </div>
         )
     }
